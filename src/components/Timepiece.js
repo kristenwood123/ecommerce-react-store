b1413ce88629416fba5bdb435b65f6f041b08bc3
Kristen <kristenwood123@gmail.com>
2021-02-07 17:36:16 -0800

    added incart styling

--- src/components/Timepiece.js
+++ src/components/Timepiece.js
@@ -12,14 +12,28 @@ export default class Timepiece extends React.Component {
         <div className="Timepiece__card">
           <div className="Timepiece__img-container" onClick={() => console.log('you clicked')}>
           <Link to='/details'>
-            <img src={img} alt="product" className='Timepiece__card'/>
+            <img src={img} alt="product" className='Timepiece__card-watch'/>
           </Link>
           </div>
           <div className="Timepiece__title">{title}</div>
-          <div className="Timepiece__price">${price}.00</div>
-          <button className='Timepiece__cart-btn' disabled={inCart ? true : false} onClick={() => {console.log('added to cart')}}></button>
         </div>
-      </div>
+
+          <button 
+            className='Timepiece__cart-btn' 
+            disabled={inCart ? true : false} 
+            onClick={() => {console.log('added to cart')
+            }} 
+            >{inCart ? (
+            <p className='disabled' disabled>
+            {'IN CART'}
+            </p>
+            ) : (
+              <p className='enabled'>ADD TO CART</p>
+            )}</button>
+          
+          <div className="Timepiece__price">${price}.00</div>
+        </div>
+      
     )
   }
 }
