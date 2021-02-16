dcbfa93a202fca31728500f17f8575bfbdd549a1
Kristen <kristenwood123@gmail.com>
2021-02-16 10:08:41 -0800

    added decrement

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
+import { prettyDOM } from '@testing-library/react'
 import React, { Component } from 'react'
 import { storeTimepieces, detailProduct } from './data'
 
@@ -31,7 +32,19 @@ const ProductContext = React.createContext()
     }
 
     decrement = (id) => {
-      console.log('this is decrement method')
+      let tempCart = [...this.state.cart]
+      const selectedProduct = tempCart.find(item => item.id == id)
+
+      const index = tempCart.indexOf(selectedProduct)
+      const product = tempCart[index]
+      product.count = product.count - 1;
+
+      if(product.count === 0){
+        this.removeItem(id)
+      } else {
+        product.total = product.count * product.price;
+        this.setState(() => {return{cart:[...tempCart]}}, () => {this.addTotals()})
+      }
     }
 
     removeItem = (id) => {
