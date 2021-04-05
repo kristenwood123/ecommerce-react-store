dec5f5862eef8eb7d8b5fd0de38039abcc717469
Kristen <kristenwood123@gmail.com>
2021-04-05 09:20:32 -0700

    changed folder name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TimepieceList from './components/TimepieceList'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Default from './components/Default'
-// import Cart from './components/cart/Cart'
+import Cart from './components/checkout/Cart'
 
 
 const App = () => {
